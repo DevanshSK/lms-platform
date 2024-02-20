0bfd578b26cad4fd9dd8af9b482502bce8116d10
Devansh Dixit <102344965+DevanshDixitDD@users.noreply.github.com>
2024-02-20 13:45:29 +0530

    I have completed the about us and contact page and made them responsive.

---
+++ app/(home)/_components/contact/contactform.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+
+const ContactForm = () => {
+  return (
+    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
+    {/* LEFT */}
+    
+    <div className="relative z-20 flex flex-1 flex-col xl:w-1/2 pt-20">
+      {/* <Image
+        src="/"
+        alt="addlearn"
+        width={50}
+        height={50}
+        className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+      /> */}
+      <h1 className="bold-32 lg:bold-88">
+        Cantact Us
+      </h1>
+      <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+      We're here to help and eager to hear from you. 
+      Whether you have questions about our services, what we do, 
+      need assistance with your account, or just want 
+      to share feedback, we're ready to assist you every step of the way.
+      </p>
+      <div className="my-11 flex flex-wrap gap-5">
+      </div>
+    </div>
+    <div className="relative flex flex-1 items-start aspect-square">
+    <form action="https://formbold.com/s/FORM_ID" method="POST" className="w-3/4">
+      <div className="mb-5">
+        <label
+          className="mb-3 block text-base font-medium text-[#07074D]"
+        >
+          Full Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Full Name"
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+        />
+      </div>
+      <div className="mb-5">
+        <label
+          className="mb-3 block text-base font-medium text-[#07074D]"
+        >
+          Email Address
+        </label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="example@domain.com"
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+        />
+      </div>
+      {/* <div className="mb-5">
+        <label
+          className="mb-3 block text-base font-medium text-[#07074D]"
+        >
+          Subject
+        </label>
+        <input
+          type="text"
+          name="subject"
+          id="subject"
+          placeholder="Enter your subject"
+          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+        />
+      </div> */}
+      <div className="mb-5">
+        <label
+          className="mb-3 block text-base font-medium text-[#07074D]"
+        >
+          Message
+        </label>
+        <textarea
+          name="message"
+          id="message"
+          placeholder="Type your message"
+          className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+        ></textarea>
+      </div>
+      <div>
+        <button
+          className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+    </div>
+  </section>
+  );
+};
+
+export default ContactForm;
--- app/(home)/_constants/index.ts
+++ app/(home)/_constants/index.ts
@@ -2,9 +2,9 @@
 export const NAV_LINKS = [
     { href: '/', key: 'home', label: 'Home' },
     { href: '/', key: 'how_addlearn_work', label: 'How Add Learn Work?' },
-    { href: '/', key: 'aboutus', label: 'AboutUs' },
+    { href: '/about', key: 'aboutus', label: 'AboutUs' },
     { href: '/', key: 'courses ', label: 'Courses' },
-    { href: '/', key: 'contact_us', label: 'Contact Us' },
+    { href: '/contact', key: 'contact_us', label: 'Contact Us' },
   ];
   
   // COURSES SECTION
