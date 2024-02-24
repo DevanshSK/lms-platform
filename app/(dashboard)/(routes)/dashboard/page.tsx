c8b1ddbcdbee186a2ebeebdfb88b6831ed4bb3d2
DevanshSK <devanshs843@gmail.com>
2024-02-24 15:41:36 +0530

    fix(query): Fixed the dunamic queryies in student enrollments.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useGetCategoriesQuery } from '@/redux/features/category/categoryApiSlice';
-import React from 'react'
+import React, { useEffect } from 'react'
 import Categories from '@/components/categories';
 import SearchInput from '@/components/search-input';
 import { useSearchParams } from 'next/navigation';
@@ -9,21 +9,19 @@ import { useGetAllCoursesWithParamsQuery } from '@/redux/features/courses/course
 import CoursesList from '@/components/courses-list';
 import { useGetEnrollmentsQuery } from '@/redux/features/user/userApiSlice';
 
-interface SearchPageProps {
-  searchParams: {
-    title: string;
-    categoryId: number;
-  }
-}
 
-const SearchPage = ({
-  searchParams
-}: SearchPageProps) => {
+
+const SearchPage = () => {
   const params = useSearchParams();
+  const title =  params.get('title') || "";
+  const categoryId =  params.get('categoryId') ? Number(params.get("categoryId")) : undefined;
   const { data: categories, isLoading: isCategoriesLoading, isError, error } = useGetCategoriesQuery();
-  const { data: courses = [], isLoading: isCourseLoading } = useGetAllCoursesWithParamsQuery({ title: searchParams.title, categoryId: searchParams.categoryId });
+  const { data: courses = [], isLoading: isCourseLoading, refetch, error: CourseError, isError: isCourseError } = useGetAllCoursesWithParamsQuery({ title: title, categoryId: categoryId });
   const { data: enrollments = [], isLoading: isEnrollmentsLoading } = useGetEnrollmentsQuery();
 
+  useEffect(() => {
+    refetch();
+}, [params, refetch]);
 
   if (isCourseLoading || isCategoriesLoading || isEnrollmentsLoading) {
     return <p className='text-center p-5 animate-pulse font-semibold'>Hang on tight, this may take a while....</p>
--- app/(home)/courses/page.tsx
+++ app/(home)/courses/page.tsx
@@ -16,21 +16,19 @@ interface SearchPageProps {
     }
 }
 
-const SearchPage = ({
-    searchParams
-}: SearchPageProps) => {
+const SearchPage = () => {
     const params = useSearchParams();
 
     const title =  params.get('title') || "";
     const categoryId =  params.get('categoryId') ? Number(params.get("categoryId")) : undefined;
     console.log(title, categoryId)
-    console.log(searchParams.title, searchParams.categoryId)
+    // console.log(searchParams.title, searchParams.categoryId)
     const { data: categories=[], isLoading: isCategoriesLoading, isError, error } = useGetCategoriesQuery();
     const { data: courses = [], isLoading: isCourseLoading, refetch, error: CourseError, isError: isCourseError } = useGetAllCoursesWithParamsQuery({ title: title, categoryId: categoryId });
 
     useEffect(() => {
         refetch();
-    }, [searchParams, params, refetch]);
+    }, [params, refetch]);
 
     if (isCourseLoading || isCategoriesLoading) {
         return <p className='text-center p-5 animate-pulse font-semibold'>Hang on tight, this may take a while....</p>
