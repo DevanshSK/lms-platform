3b65b70c1fa83b917d0ae64882b18dc25d03edb5
DevanshSK <devanshs843@gmail.com>
2024-02-24 17:53:15 +0530

    feat(email): Email functionality added

--- app/(home)/_components/contact/contactform.tsx
+++ app/(home)/_components/contact/contactform.tsx
@@ -1,5 +1,6 @@
 "use client";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 
@@ -7,83 +8,98 @@ const ContactForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const router = useRouter();
 
 
   const submitHandler = async (e: React.FormEvent) => {
     e.preventDefault();
-    try {
-      await axios.post("/api/contact", {
+    if(!name || !email || !message){
+      toast.error("Please fill all the fields");
+      return;
+    }
+    toast.promise(
+      axios.post("/api/contact", {
         name,
         email,
         message
-      });
-
-      toast.success("Email sent successfully")
-    } catch (error) {
-      console.log(error);
-      toast.error("Some error happened");
-    }
+      }),
+      {
+        loading: 'Sending Message...',
+        success: (data) => {
+          router.refresh();
+          setName("")
+          setEmail("")
+          setMessage("")
+          return "Message Sent"
+        },
+        error: (error) => {
+          console.log("Message sending error");
+          console.log(error);
+          return "Something went wrong"
+        },
+      }
+    );
   }
-  
+
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row ">
-    {/* LEFT */}
-    
-    <div className="relative z-20 flex flex-1 flex-col xl:w-1/2 pt-20">
-      {/* <Image
+      {/* LEFT */}
+
+      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2 pt-20">
+        {/* <Image
         src="/"
         alt="addlearn"
         width={50}
         height={50}
         className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
       /> */}
-      <h1 className="bold-32 lg:bold-88">
-        Contact Us
-      </h1>
-      <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
-      We&apos;re here to help and eager to hear from you. 
-      Whether you have questions about our services, what we do, 
-      need assistance with your account, or just want 
-      to share feedback, we&apos;re ready to assist you every step of the way.
-      </p>
-      <div className="my-11 flex flex-wrap gap-5">
+        <h1 className="bold-32 lg:bold-88">
+          Contact Us
+        </h1>
+        <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
+          We&apos;re here to help and eager to hear from you.
+          Whether you have questions about our services, what we do,
+          need assistance with your account, or just want
+          to share feedback, we&apos;re ready to assist you every step of the way.
+        </p>
+        <div className="my-11 flex flex-wrap gap-5">
+        </div>
       </div>
-    </div>
-    <div className="relative flex flex-1 items-start aspect-square">
-    <form onSubmit={submitHandler} className="w-3/4">
-      <div className="mb-5">
-        <label
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Full Name
-        </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Full Name"
-          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      <div className="mb-5">
-        <label
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Email Address
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="example@domain.com"
-          className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        />
-      </div>
-      {/* <div className="mb-5">
+      <div className="relative flex flex-1 items-start aspect-square">
+        <form onSubmit={submitHandler} className="w-3/4">
+          <div className="mb-5">
+            <label
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Full Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Full Name"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Email Address
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="example@domain.com"
+              className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            />
+          </div>
+          {/* <div className="mb-5">
         <label
           className="mb-3 block text-base font-medium text-[#07074D]"
         >
@@ -97,32 +113,32 @@ const ContactForm = () => {
           className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         />
       </div> */}
-      <div className="mb-5">
-        <label
-          className="mb-3 block text-base font-medium text-[#07074D]"
-        >
-          Message
-        </label>
-        <textarea
-          name="message"
-          id="message"
-          placeholder="Type your message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-        ></textarea>
+          <div className="mb-5">
+            <label
+              className="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Type your message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-full resize-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+            ></textarea>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-        <button
-        type="submit"
-          className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-base font-semibold text-white outline-none"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
-    </div>
-  </section>
+    </section>
   );
 };
 
--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,22 +1,42 @@
-import { EmailTemplate } from '@/components/email-template';
-import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+import { type NextRequest, NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
 
-export async function POST(req: Request) {
-  try {
-    const {name, email, message} = await req.json();
-    const data = await resend.emails.send({
-      from: `addtrends69@gmail.com`,
-      to: 'addtrends69@gmail.com',
-      subject: 'Message from Add Learn',
-      react: EmailTemplate({ name: name, email: email, message: message }) as React.ReactElement,
+export async function POST(request: NextRequest) {
+  const { email, name, message } = await request.json();
+
+  const transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions: Mail.Options = {
+    from: process.env.EMAIL,
+    to: process.env.MY_EMAIL,
+    cc: email,
+    subject: `Message from ADD Learning by ${name} (${email})`,
+    text: message,
+  };
+
+  const sendMailPromise = () =>
+    new Promise<string>((resolve, reject) => {
+      transport.sendMail(mailOptions, function (err) {
+        if (!err) {
+          resolve('Email sent');
+        } else {
+          reject(err.message);
+        }
+      });
     });
 
-    console.log(data)
-    return Response.json(data);
-  } catch (error) {
-    console.log(error)
-    return Response.json({ error });
+  try {
+    await sendMailPromise();
+    return NextResponse.json({ message: 'Email sent' });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 500 });
   }
 }
